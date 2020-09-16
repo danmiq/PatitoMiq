713129dab6cfe017a98727d9566e3b19f03e4e3a
danmiq <71296132+danmiq@users.noreply.github.com>
2020-09-16 11:20:48 -0300

    Delete CambiosBD.sql

--- CambiosBD.sql
+++
@@ -1,5 +0,0 @@
-UPDATE STATUS SET Nombre = 'DEVUELTA A PROVEDOR' WHERE cod_status = 4
-UPDATE STATUS SET Nombre = 'VENTA DEVUELTA' WHERE cod_status = 2
-
-alter table prendas add fec_devolucion smalldatetime null 
-
